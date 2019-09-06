a0bf287154e1924bc89a9aa56cf636aa12b6645e
andrew <andrew@I7-NUC>
2019-09-06 00:52:48 -0500

    added box drawing unicdoe chars

--- Chip8/Debugger/MemoryDumper.cs
+++ Chip8/Debugger/MemoryDumper.cs
@@ -31,9 +31,9 @@ namespace Chip8.Debugger
 
             Console.SetCursorPosition(leftpad, 0);
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"|ADDR 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
+            Console.WriteLine($"│ADDR 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
             Console.SetCursorPosition(leftpad, lines++);
-            Console.Write($"|{range.Start.Value:X4} ");
+            Console.Write($"│{range.Start.Value:X4} ");
             Console.ResetColor();
 
             for (var i = range.Start.Value; i < range.End.Value; i++)
@@ -55,14 +55,15 @@ namespace Chip8.Debugger
                     Console.WriteLine();
                     Console.SetCursorPosition(leftpad, lines++);
                     Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write($"|{range.Start.Value + (++datarow * 16):X4} ");
+                    Console.Write($"│{range.Start.Value + (++datarow * 16):X4} ");
                     Console.ResetColor();
                 }
             }
 
             Console.SetCursorPosition(leftpad, lines++);
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine(string.Empty.PadLeft(Console.BufferWidth - leftpad, '-'));
+            Console.WriteLine("└───────────────────────────┬──────────────────────────");
+            //Console.WriteLine("└".PadRight(Console.BufferWidth - leftpad, '─'));
             Console.ResetColor();
             Console.SetCursorPosition(left, top);
         }
@@ -98,12 +99,12 @@ namespace Chip8.Debugger
         {
             var buffer = new StringBuilder();
 
-            buffer.Append($"| Index = #{state.Index:X3} ");
+            buffer.Append($"│ Index = #{state.Index:X3} ");
             buffer.AppendLine($"IP = #{state.ProgramCounter:X3} ");
 
             for (var i = 0; i < 16; i++)
             {
-                buffer.AppendLine($"| V{i:X1} = #{state.Registers[i]:X2}");
+                buffer.AppendLine($"│ V{i:X1} = #{state.Registers[i]:X2}");
             }
 
             return buffer.ToString();
